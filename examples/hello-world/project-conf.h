3e1011863342df750a20b425942250cb4246561f
Damien Varvenne <dv@seneco.dk>
2021-02-01 18:34:20 +0100

    setup to reproduce issue with RPL lite and UIP_CONF_ND6_SEND_NS=1

---
+++ examples/hello-world/project-conf.h
@@ -0,0 +1,15 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+/* Enable send NS */
+#define UIP_CONF_ND6_SEND_NS                    1
+//#define UIP_CONF_ND6_AUTOFILL_NBR_CACHE         1
+
+/*---------------------------------------------------------------------------*/
+/* Log */
+#ifndef LOG_CONF_LEVEL_RPL
+#define LOG_CONF_LEVEL_RPL              LOG_LEVEL_INFO
+#endif
+/*---------------------------------------------------------------------------*/
+#endif /* PROJECT_CONF_H_ */
+/*---------------------------------------------------------------------------*/
